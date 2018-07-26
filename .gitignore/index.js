b7bc2675d82953f5e046a90980db2c6f9aeb233b
WhitePistFR <40545506+WhitePistFR@users.noreply.github.com>
2018-07-26 13:27:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,21 +9,6 @@ const queue = new Map();
 var servers = {};
 
 client.login(process.env.TOKEN);
-
-function play(connection, message) {
-  
-  var server = servers[message.guild.id];
-
-  server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-
-  server.queue.shift();
-
-  server.dispatcher.on("end", function() { 
-    if (server.queue[0]) play(connection, message);
-
-    else connection.disconnect();
-
-  });
 }
 
 client.on("ready", () => {
